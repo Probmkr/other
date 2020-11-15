de47fc5a12f8e26af426e1a7253033886e99d4de
Thanatos9911 <thanatos9911@gmail.com>
2020-11-15 15:17:35 +0900

    add no recurasion function

--- Python/algorithm/recursion/mountain.py
+++ Python/algorithm/recursion/mountain.py
@@ -6,9 +6,19 @@ def layer(max, i = 1):
         print('*' * i)
     return i - 1
 
+def layer_not_recursion(max, i = 1):
+    for j in range(i, max + 1):
+        print('*' * j)
+    for j in reversed(range(i, max)):
+        print('*' * j)
+
 if __name__=='__main__':
 
     max = int(input('>> '))
+    try:
+        i = int(input('>> '))
+    except ValueError:
+        i = 1
     print('\n\n')
 
-    layer(max)
+    layer(max, i)

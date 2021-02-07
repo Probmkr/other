d9f221ec713da699e2de12545ce61c973be0a8d3
Thanatos9911 <Thanatos9911@gmail.com>
2021-02-07 22:43:44 +0900

    update

---
+++ C#/Helllo World/Hello_World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorld
+{
+    class MAIN
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nHello World");
+        }
+    }
+}

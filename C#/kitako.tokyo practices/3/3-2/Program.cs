b110e45a950fe1318fef42cb1a1ef7cbb9445946
Thanatos9911 <Thanatos9911@gmail.com>
2021-03-07 22:49:10 +0900

    update C# practice programs

---
+++ C#/kitako.tokyo practices/3/3-2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Console.Write(" > ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write(" > ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x >= y)
+            {
+                Console.WriteLine("\n{0}", x);
+            }
+            else
+            {
+                Console.WriteLine("\n{0}", y);
+            }
+        }
+    }
+}

1b2ff1835234eb8703877771d0eba7374fc11ae0
Thanatos9911 <Thanatos9911@gmail.com>
2021-03-10 22:56:51 +0900

    update

---
+++ C#/kitako.tokyo practices/4/4-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1;
+            for (int i = 1; i <= 7; i++)
+            {
+                result *= i;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

2830ecc14379b3347a5e3f8d9086222550beab9f
Thanatos9911 <Thanatos9911@gmail.com>
2021-03-11 21:18:54 +0900

    update

---
+++ C#/Practice/LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {20, 420, 560 ,315, 290, 600};
+
+            var newArray = array.Where(n => n >= 500);
+
+            foreach (int n in newArray)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ C#/Practice/Lambda/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            Func<int, int> func = (num) => num + 10;
+            n = func(n);
+            Console.WriteLine(n);
+        }
+    }
+}

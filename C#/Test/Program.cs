dec26f6871f25f359e5be95a3ae0c2722fe3b871
Thanatos9911 <Thanatos9911@gmail.com>
2021-03-22 22:34:13 +0900

    update

--- C#/Test/Program.cs
+++ C#/Test/Program.cs
@@ -6,7 +6,37 @@ namespace Test
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numQuant = int.Parse(Console.ReadLine());
+            int forTime = numQuant - 1;
+            string[] input = Console.ReadLine().Split(' ');
+            int[] numArr = new int[numQuant];
+            for (int i = 0; i < forTime; i++)
+            {
+                numArr[i] = int.Parse(input[i]);
+            }
+
+            bool canDupMore = true;
+            int count = 0;
+
+            while(canDupMore)
+            {
+                count++;
+                for (int i = 0; i < forTime; i++)
+                {
+                    if (numArr[i] % 2 == 0)
+                    {
+                        numArr[i] /= 2;
+                    }
+                    else
+                    {
+                        canDupMore = false;
+                        count--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(count);
         }
     }
 }

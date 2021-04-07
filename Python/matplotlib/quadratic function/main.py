1df02c19c3877316f13e11cf9f1a7933c3ed4f7c
Thanatos9911 <Thanatos9911@gmail.com>
2021-04-07 22:13:48 +0900

    update

--- Python/matplotlib/quadratic function/main.py
+++ Python/matplotlib/quadratic function/main.py
@@ -1,1 +1,10 @@
-import matplotlib
+from matplotlib import pyplot
+
+x = [i for i in range(-100, 101)]
+y = [i**2 for i in range(-100, 101)]
+
+pyplot.plot(x, y)
+pyplot.plot([0, 0], [-1000, 11000], color='black', linestyle='dashed')
+pyplot.plot([-150, 150], [0, 0], color='black', linestyle='dashed')
+
+pyplot.show()

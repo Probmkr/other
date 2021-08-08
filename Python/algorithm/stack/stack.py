ee2a4375910af132f7d481ae9d174fbd35948302
Thanatos9911 <Thanatos9911@gmail.com>
2021-08-08 12:03:25 +0900

    update

---
+++ Python/algorithm/stack/stack.py
@@ -0,0 +1,28 @@
+from typing import Any
+
+class FixedStack:
+    class StackEmpty(Exception):
+        pass
+
+    class StackFull(Exception):
+        pass
+
+    def __init__(self, capacity: int = 256) -> None:
+        self.stk = [None] * capacity
+        self.capacity = capacity
+        self.ptr = 0
+
+    def __len__(self) -> int:
+        return self.ptr
+
+    def is_empty(self) -> bool:
+        return self.ptr <= 0
+
+    def is_full(self) -> bool:
+        return self.ptr >= self.capacity
+
+    def push(self, value: Any) -> None:
+        if self.is_full():
+            raise FixedStack.StackEmpty
+        self.stk[self.ptr] = value
+        self.ptr += 1

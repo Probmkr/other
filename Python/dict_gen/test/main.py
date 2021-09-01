0d6f2b8400cb0e37ea9ad8b21770ab45aa218981
= <=>
2021-09-01 22:35:33 +0900

    update

---
+++ Python/dict_gen/test/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gen(length):
+    dictlist = []
+    gen_one(length, dictlist)
+
+def gen_one(length, dictlist):
+
+
+dictcol = 20
+for i in range(dictcol):
+    for k in range(0x20, 0x7f):
+        for j in range(i+1):
+            print(bytearray.fromhex(np.base_repr(i, 16)).decode())
